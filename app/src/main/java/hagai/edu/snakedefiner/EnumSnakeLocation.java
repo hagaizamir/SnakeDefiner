b86abf1e7da3e809b7173306a3b90c667369d6ed
Hagai Zamir <hagai34@gmail.com>
2017-08-15 11:52:37 +0300

    enum color and location update+opening xml

--- app/src/main/java/hagai/edu/snakedefiner/EnumSnakeLocation.java
+++ app/src/main/java/hagai/edu/snakedefiner/EnumSnakeLocation.java
@@ -1,8 +1,31 @@
 package hagai.edu.snakedefiner;
 
+import android.content.Context;
+
 /**
  * Created by Hagai Zamir on 11-Aug-17.
  */
 
 public enum EnumSnakeLocation {
+
+    snakeLocationUndefined (R.string.snake_location_undefined),
+    snakeLocationNegevArava (R.string.snake_location_negev_arava),
+    snakeLocationJudaDeserd (R.string.snake_location_juda_deserd),
+    snakeLocationJudaSamaria (R.string.snake_location_juda_samaria),
+    snakeLocationGalil (R.string.snake_location_galil),
+    snakeLocationGlolaHights (R.string.snake_location_golan_hights),
+    snakeLocationCoastLine (R.string.snake_location_coast_line);
+
+    private int snakeLocationStringID;
+
+
+    EnumSnakeLocation(int snakeLocationStringID) {
+        this.snakeLocationStringID = snakeLocationStringID;
+    }
+
+    static String getSnakeLocationStringID(EnumSnakeLocation snakeLocation , Context context) {
+        return context.getString(snakeLocation.snakeLocationStringID);
+    }
+
+
 }
