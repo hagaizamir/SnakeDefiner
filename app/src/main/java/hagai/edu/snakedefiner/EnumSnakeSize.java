6ad693204b987009a7c97ad187a178c2bdba6972
Hagai Zamir <hagai34@gmail.com>
2017-08-15 13:14:36 +0300

    enum color , size and location update+opening xml

--- app/src/main/java/hagai/edu/snakedefiner/EnumSnakeSize.java
+++ app/src/main/java/hagai/edu/snakedefiner/EnumSnakeSize.java
@@ -1,8 +1,25 @@
 package hagai.edu.snakedefiner;
 
+import android.content.Context;
+
 /**
  * Created by Hagai Zamir on 11-Aug-17.
  */
 
 public enum EnumSnakeSize {
+    snakeSizeUndefined (R.string.snake_size_undefined),
+    snakeSizeSmall (R.string.snake_size_small),
+    snakeSizeMedium (R.string.snake_size_medium),
+    snakeSizeLarge (R.string.snake_size_large),
+    snakeSizeExtraLarge (R.string.snake_size_extra_large);
+
+    private int snakeSizeStringID;
+
+    EnumSnakeSize(int snakeSizeStringID) {
+        this.snakeSizeStringID = snakeSizeStringID;
+    }
+
+    public String getSnakeSize(int snakeSizeStringID , Context context) {
+        return context.getString(snakeSizeStringID) ;
+    }
 }
