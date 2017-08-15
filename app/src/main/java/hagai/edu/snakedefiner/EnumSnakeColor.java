c3fbf019cff295b1ec816382356106ddd833bed8
Hagai Zamir <hagai34@gmail.com>
2017-08-15 09:58:30 +0300

    enum color update

--- app/src/main/java/hagai/edu/snakedefiner/EnumSnakeColor.java
+++ app/src/main/java/hagai/edu/snakedefiner/EnumSnakeColor.java
@@ -8,8 +8,18 @@ import android.content.Context;
 
 public enum EnumSnakeColor {
     snakeColorUndefined (R.string.snake_color_undefined),
-    snakeColorBrownRed(R.string.snake_color_brown_red),
+    snakeColorOrangeRed(R.string.snake_color_orange_red),
+    snakeColorLightBrwon(R.string.snake_color_lightbrown),
+    snakeColorWhite(R.string.snake_color_white),
+    snakeColorYellow(R.string.snake_color_yellow),
+    snakeColorGreen(R.string.snake_color_green),
+    snakeColorGray(R.string.snake_color_gray),
+    snakeColorDarkBrown(R.string.snake_color_darkbrown),
+    snakeColorPink(R.string.snake_color_pink),
+    snakeColorBluish(R.string.snake_color_bluish),
     snakeColorBlack(R.string.snake_color_black);
+
+
 
     private int snakeColorStringID;
 
@@ -17,6 +27,8 @@ public enum EnumSnakeColor {
     EnumSnakeColor(int snakeColorStringID) {
         this.snakeColorStringID = snakeColorStringID;
     }
+
+
 
     static String getSnakeColor(EnumSnakeColor snakeColor, Context context) {
         return  context.getString(snakeColor.snakeColorStringID);
